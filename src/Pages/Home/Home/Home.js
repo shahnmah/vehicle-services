5ba5bf7480e1060e158073936256c821e748dc58
shahnmah <shahn.mah@gmail.com>
2022-04-11 00:48:35 +0600

    services data added

---
+++ src/Pages/Home/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.css'
+import Services from '../Services/Services'
+
+const Home = () => {
+    return (
+        <div>
+            <h2>Home is where the heart is</h2>
+            <Services></Services>
+        </div>
+    );
+};
+
+export default Home;
