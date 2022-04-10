f28225357a32aa5d266946018cbea5a9a3df961c
shahnmah <shahn.mah@gmail.com>
2022-04-11 02:48:36 +0600

    banner added

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './Home.css'
 import Services from '../Services/Services'
 import Experts from '../Experts/Experts';
+import Banner from '../Banner/Banner';
 
 const Home = () => {
     return (
         <div>
+            <Banner></Banner>
             <Services></Services>
             <Experts></Experts>
         </div>
