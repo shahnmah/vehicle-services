ef839fd8f533bf291d1eca97523d6a132b975cd5
shahnmah <shahn.mah@gmail.com>
2022-04-14 23:24:32 +0600

    google sign in added

--- src/Pages/Login/SocialLogin/SocialLogin.js
+++ src/Pages/Login/SocialLogin/SocialLogin.js
@@ -2,16 +2,33 @@ import React from 'react';
 import googleIcon from '../../../images/social/google.png'
 import facebookIcon from '../../../images/social/facebook.png'
 import githubIcon from '../../../images/social/github.png'
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
+import { useNavigate } from 'react-router-dom';
+
+
 const SocialLogin = () => {
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    const navigate = useNavigate()
+    let errorElement;
+    if (error) {
+        errorElement = <div>
+            <p className='text-danger'>Error: {error.message}</p>
+        </div>
+    }
+    if (user) {
+        navigate('/home')
+    }
     return (
         <div>
             <div className='d-flex align-items-center'>
-                <div style={{height: '1px'}} className='bg-primary w-50'></div>
+                <div style={{ height: '1px' }} className='bg-primary w-50'></div>
                 <p className='mt-2 px-2'>or</p>
-                <div style={{height: '1px'}} className='bg-primary w-50'></div>
+                <div style={{ height: '1px' }} className='bg-primary w-50'></div>
             </div>
+            {errorElement}
             <div>
-                <button className='btn btn-info w-50 d-block mx-auto'>
+                <button onClick={() => signInWithGoogle()} className='btn btn-info w-50 d-block mx-auto'>
                     <img src={googleIcon} alt="" />
                     <span className='px-2'>Sign In With Google</span>
                 </button>
